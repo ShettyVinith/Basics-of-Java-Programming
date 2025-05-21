0b4e1603f688c7dcba046b3817fcb7928af1a020
ShettyVinith <vinithshetty9372@gmail.com>
2025-05-21 23:19:06 +0530

    Added Questions

--- 4a_Basic Hello Swing Program/HelloSwing.java
+++ 4a_Basic Hello Swing Program/HelloSwing.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class HelloSwing {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Welcome");
-        JLabel label = new JLabel("Hello! VI C , Welcome to Swing Programming", SwingConstants.CENTER);
+        JLabel label = new JLabel("Hello! Vinith , Welcome to Swing Programming", SwingConstants.CENTER);
 
         label.setFont(new Font("SansSerif", Font.PLAIN, 32));
         label.setForeground(Color.BLUE);
