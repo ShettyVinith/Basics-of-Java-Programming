a676891a0d5d670b24813b91586471a876e9dba5
ShettyVinith <vinithshetty9372@gmail.com>
2025-05-21 16:44:20 +0530

    Added Questions

--- 6c_Servlet Prime Number Check/src/main/java/servlet/PrimeServlet.java
+++ 6c_Servlet Prime Number Check/src/main/java/servlet/PrimeServlet.java
@@ -1,6 +1,7 @@
+/*6c. Build a servlet program to check the given number is prime number or not using HTML with step
+by step procedure. */
+
 package servlet;
-
-
 
 import java.io.IOException;
 import java.io.PrintWriter;
